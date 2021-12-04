06479334cf8ac1f6fdd2173a96a147c2d334fd9d
risfannovrian18 <risfan.unida@unida.ac.id>
2021-12-04 11:24:55 +0700

    First Commit

---
+++ src/Switchcase.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Switchcase {
+	public static void main (String[] args) {
+		int day;
+		System.out.println("Masukkan Angka 1-7");
+		Scanner masukan = new Scanner (System.in);
+		day = masukan.nextInt();
+		switch (day) {
+		case 1:
+			System.out.println ("Senin");
+			break;
+		case 2:
+			System.out.println ("Selasa");
+			break;
+		case 3:
+			System.out.println ("Rabu");
+			break;
+		case 4:
+			System.out.println ("Kamis");
+			break;
+		case 5:
+			System.out.println ("Jum'at");
+		case 6:
+			System.out.println ("Sabtu");
+			break;
+		case 7:
+			System.out.println ("Minggu");
+			break;
+		default:
+			System.out.println ("Tidak ada hari");
+		}
+	}
+}
